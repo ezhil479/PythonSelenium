2d22e1637ea162b3f562ec03ef37654c46ac2423
ezhil479 <ezhilarasiece1992@gmail.com>
2023-11-02 15:37:26 +0530

    updated

---
+++ demo.py
@@ -0,0 +1,17 @@
+name = 12
+print(name)
+name = 12.0
+print(name)
+var1 = 90
+var2 = 19.5
+result = var1 + var2
+print(result)
+
+
+status = True
+print(status)
+
+
+status = None  # none, true not accepted python is case-sensitive
+print(status)
+
